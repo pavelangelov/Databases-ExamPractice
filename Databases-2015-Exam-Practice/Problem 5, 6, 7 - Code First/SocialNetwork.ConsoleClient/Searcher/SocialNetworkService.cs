7fa81e86810385f779eb0974b4aaff51c8e5e482
pavelangelov <p_a_v_el@abv.bg>
2016-11-07 11:05:15 +0200

    Added searching queries

---
+++ Databases-2015-Exam-Practice/Problem 5, 6, 7 - Code First/SocialNetwork.ConsoleClient/Searcher/SocialNetworkService.cs
@@ -0,0 +1,75 @@
+﻿using SocialNetwork.Data;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.ConsoleClient.Searcher
+{
+    public class SocialNetworkService : ISocialNetworkService
+    {
+        private SocialNetworkDBContext context;
+
+        public SocialNetworkService(SocialNetworkDBContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentException("The database context cannot be null!");
+            }
+
+            this.context = context;
+        }
+
+        public IEnumerable GetChatUsers(string username)
+        {
+            return null;
+        }
+
+        public IEnumerable GetFriendships(int page = 1, int pageSize = 25)
+        {
+            var friendships = context.Friendships
+                                        .Skip((page - 1) * pageSize)
+                                        .Take(pageSize)
+                                        .Select(f => new
+                                        {
+                                            FirstUserUsername = f.FirstUser.UserName,
+                                            FirstUserImage = f.FirstUser.Images.Select(i => i.Url).FirstOrDefault(),
+                                            SecondUserUsername = f.SecondUser.UserName,
+                                            SecondUserImage = f.SecondUser.Images.Select(i => i.Url).FirstOrDefault()
+                                        })
+                                        .ToList();
+
+            return friendships;
+        }
+
+        public IEnumerable GetPostsByUser(string username)
+        {
+            var posts = context.Posts.Where(p => p.TaggedUsers.Any(u => u.UserName == username))
+                                        .Select(p => new
+                                        {
+                                            p.PostingDate,
+                                            p.Content,
+                                            Users = p.TaggedUsers.Select(u => u.UserName)
+                                        })
+                                        .ToList();
+
+            return posts;
+        }
+
+        public IEnumerable GetUsersAfterCertainDate(int year)
+        {
+            var users = context.UserProfiles.Where(u => u.RegistrationDate.Year >= year)
+                                            .Select(u => new
+                                            {
+                                                Username = u.UserName,
+                                                FirstName = u.FirstName,
+                                                LastName = u.LastName,
+                                                Images = u.Images.Count
+                                            });
+
+            return users;
+        }
+    }
+}
